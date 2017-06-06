d25b4024644fc6632c812d07861053e9ebe23fef
Andy Evans <andrewevans0@outlook.com>
2017-06-06 20:23:12 +0100

    now moves forwards and disables the pins

---
+++ src/main/java/hello/Forwards.java
@@ -0,0 +1,61 @@
+package hello;
+
+import com.pi4j.io.gpio.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andrewevans on 06/06/2017.
+ */
+public class Forwards {
+    public static void moveForwards() throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+
+        final GpioController gpio = GpioFactory.getInstance();
+
+        final GpioPinDigitalOutput motor1A = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput motor1B = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput motor1E = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.HIGH);
+
+        final GpioPinDigitalOutput motor2A = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput motor2B = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput motor2E = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.HIGH);
+
+        List<GpioPinDigitalOutput> pins = new ArrayList<>();
+
+        pins.add(motor1A);
+        pins.add(motor1B);
+        pins.add(motor1E);
+        pins.add(motor2A);
+        pins.add(motor2B);
+        pins.add(motor2E);
+
+        // set shutdown state for this pin
+        motor1A.setShutdownOptions(true, PinState.LOW);
+        motor1B.setShutdownOptions(true, PinState.LOW);
+        motor1E.setShutdownOptions(true, PinState.LOW);
+
+        motor2A.setShutdownOptions(true, PinState.LOW);
+        motor2B.setShutdownOptions(true, PinState.LOW);
+        motor2E.setShutdownOptions(true, PinState.LOW);
+
+        System.out.println("--> GPIO state should be: ON");
+
+        motor1A.high();
+        motor1B.low();
+        motor1E.high();
+
+        motor2A.high();
+        motor2B.low();
+        motor2E.high();
+
+        Thread.sleep(5000);
+
+        gpio.shutdown();
+        //shut down the pins for reuse
+        for (GpioPinDigitalOutput pin : pins) gpio.unprovisionPin(pin);
+
+        System.out.println("Exiting ControlGpioExample");
+    }
+}
--- src/main/java/hello/ForwardsController.java
+++ src/main/java/hello/ForwardsController.java
@@ -1,12 +1,9 @@
 package hello;
 
-import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 //GPIO imports
@@ -23,54 +20,7 @@ public class ForwardsController {
     @RequestMapping("/hits/forwards")
     public Response response(@RequestParam(value="name", defaultValue="World") String name) throws InterruptedException {
 
-        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
-
-        final GpioController gpio = GpioFactory.getInstance();
-
-        final GpioPinDigitalOutput motor1A = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED", PinState.HIGH);
-        final GpioPinDigitalOutput motor1B = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "MyLED", PinState.HIGH);
-        final GpioPinDigitalOutput motor1E = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.HIGH);
-
-        final GpioPinDigitalOutput motor2A = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.HIGH);
-        final GpioPinDigitalOutput motor2B = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.HIGH);
-        final GpioPinDigitalOutput motor2E = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "MyLED", PinState.HIGH);
-
-        List<GpioPinDigitalOutput> pins = new ArrayList<>();
-
-        pins.add(motor1A);
-        pins.add(motor1B);
-        pins.add(motor1E);
-        pins.add(motor2A);
-        pins.add(motor2B);
-        pins.add(motor2E);
-
-        // set shutdown state for this pin
-        motor1A.setShutdownOptions(true, PinState.LOW);
-        motor1B.setShutdownOptions(true, PinState.LOW);
-        motor1E.setShutdownOptions(true, PinState.LOW);
-
-        motor2A.setShutdownOptions(true, PinState.LOW);
-        motor2B.setShutdownOptions(true, PinState.LOW);
-        motor2E.setShutdownOptions(true, PinState.LOW);
-
-        System.out.println("--> GPIO state should be: ON");
-
-        motor1A.high();
-        motor1B.low();
-        motor1E.high();
-
-        motor2A.high();
-        motor2B.low();
-        motor2E.high();
-
-        Thread.sleep(5000);
-
-        gpio.shutdown();
-        //shut down the pins for reuse
-        for (GpioPinDigitalOutput pin : pins) gpio.unprovisionPin(pin);
-
-        System.out.println("Exiting ControlGpioExample");
-
+        Forwards.moveForwards();
 
         return new Response(counter.incrementAndGet(),
                 String.format(template, name));
