fac304011130554a144e5d8a404ef20475c53981
Andy Evans <andrewevans0@outlook.com>
2017-06-12 21:11:23 +0100

    jar will just be run now

--- init.sh
+++ init.sh
@@ -5,6 +5,5 @@ cd /home/pi/
 cd Documents/
 cd springpi/
 cd pirestservice/
-/opt/apache-maven-3.2.5/bin/mvn clean install
 cd target
 sudo java -jar gs-rest-service-0.1.0.jar
--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@ cd Documents/
 cd springpi/
 cd pirestservice/
 git pull
+/opt/apache-maven-3.2.5/bin/mvn clean install
