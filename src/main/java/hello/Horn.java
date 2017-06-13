7116d7cac90b040cbaafd030d8d813fc2877671d
Andy Evans <andrewevans0@outlook.com>
2017-06-13 18:47:20 +0100

    jar will just be run now

---
+++ src/main/java/hello/Horn.java
@@ -0,0 +1,46 @@
+package hello;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.io.gpio.impl.GpioControllerImpl;
+
+/**
+ * Created by andrewevans on 13/06/2017.
+ */
+public class Horn {
+    protected void finalize() {
+        System.out.println("last cleanup!");
+    }
+    public void soundHorn() throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+
+        //must not be static!!!!
+        final GpioController gpio = new GpioControllerImpl();
+
+        final GpioPinDigitalOutput horn = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+
+        System.out.println("--> GPIO state should be: ON");
+
+        horn.high();
+
+        horn.setShutdownOptions(true, PinState.LOW, PinPullResistance.OFF);
+
+        // set shutdown state for this pin
+
+
+        System.out.println("before thread");
+        Thread.sleep(1000);
+        System.out.println("after thread");
+        System.out.println(Thread.activeCount());
+        System.out.println("before second shutdown options");
+        //shut down the pins for reuse
+        System.out.println("has the gpio shutdown correctly?");
+        System.out.println(gpio.isShutdown());
+        System.out.println("shutting down");
+        gpio.shutdown();
+        System.out.println("post shutdown");
+        System.out.println(gpio.isShutdown());
+        gpio.unprovisionPin(horn);
+        System.out.println("should be off now!");
+        System.gc();
+    }
+}
