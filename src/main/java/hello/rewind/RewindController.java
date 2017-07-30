7074f4f4793497c8694aa1085655cdcd8694c9ba
Andy Evans <andrewevans0@outlook.com>
2017-07-30 09:00:40 +0100

    debugging

--- src/main/java/hello/rewind/RewindController.java
+++ src/main/java/hello/rewind/RewindController.java
@@ -1,6 +1,5 @@
 package hello.rewind;
 
-import hello.movement.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -19,14 +19,16 @@ public class RewindController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/hits/rewind")
-    public List<Direction> response(@RequestParam(value="name", defaultValue="World") String name) throws InterruptedException {
+    public List<LinkedHashMap> response(@RequestParam(value="name", defaultValue="World") String name) throws InterruptedException {
 
         RestTemplate restTemplate = new RestTemplate();
-        List<Direction> previousRequests = restTemplate.getForObject("http://localhost:8080/rewind/all", List.class);
+        List<LinkedHashMap> previousRequests = restTemplate.getForObject("http://localhost:8080/rewind/all", List.class);
         log.info(previousRequests.toString());
 
-        for (Direction request : previousRequests) {
-            restTemplate.getForObject(request.getDirection(), Response.class);
+        for (LinkedHashMap request : previousRequests) {
+            log.debug("what is in the hashmap?");
+            log.debug(request.toString());
+            log.debug(request.entrySet().toString());
         }
 
         return previousRequests;
