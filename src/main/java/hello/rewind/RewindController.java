172615d7bd08fbf6a0df2a230eb35dc67a51337e
Andy Evans <andrewevans0@outlook.com>
2017-07-30 11:42:17 +0100

    More changes

--- src/main/java/hello/rewind/RewindController.java
+++ src/main/java/hello/rewind/RewindController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -19,14 +18,14 @@ public class RewindController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/hits/rewind")
-    public List<LinkedHashMap> response(@RequestParam(value="name", defaultValue="World") String name) throws InterruptedException {
+    public List<Direction> response(@RequestParam(value="name", defaultValue="World") String name) throws InterruptedException {
 
         RestTemplate restTemplate = new RestTemplate();
-        List<LinkedHashMap> previousRequests = restTemplate.getForObject("http://localhost:8080/rewind/all", List.class);
-        for (LinkedHashMap map: previousRequests) {
-            map.forEach((key, value) -> {
-                System.out.println("Key : " + key + " Value : " + value);
-            });
+        List<Direction> previousRequests = restTemplate.getForObject("http://localhost:8080/rewind/all", List.class);
+        for (Direction request: previousRequests) {
+            log.debug("direction");
+            log.debug(request.getDirection());
+            restTemplate.getForObject(request.getDirection(), Object.class);
         }
 
 
