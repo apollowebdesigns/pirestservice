a5e6cfaa7f87e13b8c1275a3e690f9f538c5c80a
Andy Evans <andrewevans0@outlook.com>
2017-08-21 21:30:59 +0100

    update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,6 @@
 package hello;
 
+import hello.email.IpAddressFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,34 +29,6 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-
-        /*
-String commands
-String cleanTable = "DROP TABLE customers IF EXISTS";
-String createTable = "CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))";
-String getIds = "SELECT id, first_name, last_name FROM customers WHERE first_name = ?";
-
-log.info("Creating tables");
-
-jdbcTemplate.execute(cleanTable);
-jdbcTemplate.execute(createTable);
-
-// Split up the array of whole names into an array of first/last names
-List<Object[]> splitUpNames = Arrays.asList("hits/forwards blah", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-.map(name -> name.split(" "))
-.collect(Collectors.toList());
-
-// Use a Java 8 stream to print out each tuple of the list
-splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-
-// Uses JdbcTemplate's batchUpdate operation to bulk load data
-jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
-
-log.info("Querying for customer records where first_name = 'Josh':");
-jdbcTemplate.query(
-getIds, new Object[] { "Josh" },
-(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-).forEach(customer -> log.info(customer.toString()));
-*/
+        IpAddressFinder.getInstance().test();
     }
 }
