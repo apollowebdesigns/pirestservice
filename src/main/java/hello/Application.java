0bc7ed08df0fedccfca682b8702b718c56169d15
Andy Evans <andrewevans0@outlook.com>
2017-10-15 16:12:51 +0100

    tidying up

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -71,19 +71,20 @@ public class Application {
             hostname = ip.getHostName();
             System.out.println("Your current IP address : " + ip);
             System.out.println("Your current Hostname : " + hostname);
+            String from = "raspberrypinoreply123@gmail.com";
+            String pass = "RideThePi1993";
+            String[] to = { "andrewevans0@outlook.com" }; // list of recipient email addresses
+            String subject = "Java send mail example";
+            String body = "Welcome to Raspberry Pi! Your IP address is " + ip.getHostName();
 
+            sendFromGMail(from, pass, to, subject, body);
         } catch (UnknownHostException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("different exception");
+            e.printStackTrace();
+        } finally {
+            SpringApplication.run(Application.class, args);
         }
-
-
-        String from = "raspberrypinoreply123@gmail.com";
-        String pass = "RideThePi1993";
-        String[] to = { "andrewevans0@outlook.com" }; // list of recipient email addresses
-        String subject = "Java send mail example";
-        String body = "Welcome to Raspberry Pi! Your IP address is " + ip.getHostName();
-
-        sendFromGMail(from, pass, to, subject, body);
-        SpringApplication.run(Application.class, args);
     }
 }
